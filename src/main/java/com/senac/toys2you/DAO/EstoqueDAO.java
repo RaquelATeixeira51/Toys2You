7e3c9c66c0cb6ea8d0808173ea2b05983a557ced
RAQUEL APARECIDA TEIXEIRA <raquel.ateixeira3@senacsp.edu.br>
2022-08-26 20:31:30 -0300

    adiciona classes

---
+++ src/main/java/com/senac/toys2you/DAO/EstoqueDAO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.senac.toys2you.DAO;
+
+/**
+ *
+ * @author raquel.ateixeira3
+ */
+public class EstoqueDAO {
+    
+}
