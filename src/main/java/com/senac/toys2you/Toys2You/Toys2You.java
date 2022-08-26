fd605f0351ab911b72b20d21f70fb07154579bfe
RAQUEL APARECIDA TEIXEIRA <raquel.ateixeira3@senacsp.edu.br>
2022-08-26 19:46:10 -0300

    primeiro commit

---
+++ src/main/java/com/senac/toys2you/Toys2You/Toys2You.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.senac.toys2you.Toys2You;
+
+/**
+ *
+ * @author raquel.ateixeira3
+ */
+public class Toys2You {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
