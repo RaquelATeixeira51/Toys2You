423990868880ef7e1b3ef10d3c2ea23a3109dc33
EndriOliveira <endriojesus76@gmail.com>
2022-09-26 10:38:26 -0300

    script DB

--- src/main/java/com/senac/toys2you/DAO/MovimentoDAO.java
+++ src/main/java/com/senac/toys2you/DAO/MovimentoDAO.java
@@ -1,5 +1,13 @@
 package com.senac.toys2you.DAO;
 
-public class MovimentoDAO {
-    
+import java.sql.Connection;
+import java.util.List;
+
+import com.senac.toys2you.Model.Movimento;
+
+public interface MovimentoDAO {
+    public Connection connect(String urlConexao);
+    public void insert(String urlConexao, Movimento movimento);
+    public void delete(String urlConexao, int id);
+    public List<String> getMovimentoL(String urlConexao, String login, String senha);
 }
